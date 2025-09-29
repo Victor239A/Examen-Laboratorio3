c08a059bc1b8aef1e4dee59c08d575a4e6f65e8d
Victoor <victoraju239932@gmail.com>
2025-09-29 02:22:24 -0600

    Header

---
+++ componentes/formulario/formulario.js
@@ -0,0 +1,30 @@
+
+export function centro() {
+    let div = document.createElement("div");
+    div.className = "centro";
+    div.id = "Seccion";
+  
+    let total = document.createElement("h2");
+    total.className = "total";
+  
+    let inputProducto = document.createElement("input");
+    inputProducto.type = "text";
+    inputProducto.placeholder = "";
+  
+    
+  
+    let btn = document.createElement("button");
+    btn.textContent = "Agregar";
+  
+    let lista = document.createElement("ul");
+    lista.className = "lista";
+
+
+    div.appendChild(total);
+    div.appendChild(inputProducto);
+   
+    div.appendChild(btn);
+    div.appendChild(lista);
+    
+    return div;
+    }
---
+++ main.js
@@ -0,0 +1,9 @@
+import { centro } from "/componentes/formulario/formulario.js";
+
+
+    let section = document.createElement('section');
+
+    section.appendChild(centro());
+
+    
+    document.body.appendChild(section);
